479656f9f43993a33127a0f4badd1c36eec8036f
ho00 clouddnagmbhgmail.onmicrosoft.com <ho00@clouddnagmbhgmail.onmicrosoft.com>
2022-08-30 09:02:45 +0000

    Formatter

--- webapp/controller/Customer.controller.js
+++ webapp/controller/Customer.controller.js
@@ -10,6 +10,32 @@ sap.ui.define([
         return Controller.extend("at.clouddna.training00.zhoui5.controller.Customer", {
             onInit: function () {
 
+            },
+
+            genderFormatter: function (sGender) {
+
+                let oView = this.getView();
+                let oI18nModel = oView.getModel("i18n");
+                let oResourceBundle = oI18nModel.getResourceBundle();
+
+                switch(sGender){
+                    case "female":
+                        return oResourceBundle.getText("female");
+                    case "male":
+                        return oResourceBundle.getText("male");
+                    case "divers":
+                        return "divers";
+                    default:
+                        return "?";
+                }
+ 
+                /*
+                if(sGender === "female"){
+                    return oResourceBundle.getText("female");
+                }else if(){
+                    return oResourceBundle.getText("male");
+                }
+                */
             }
         });
     });
